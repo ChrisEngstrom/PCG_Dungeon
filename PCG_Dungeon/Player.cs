9d1f985ff02c2d8e4fff853152f9a17239937f27
Chris Engstrom <tipher88@gmail.com>
2016-05-12 00:08:11 -0500

    Added the Player class and added comments to the Dungeon class
    Changed the player variable in the Dungeon class from an instance of the
    Point class to an instance of the Player class. The Player class will
    hopefully have more functionality in the future.

---
+++ PCG_Dungeon/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCG_Dungeon {
+    /// <summary>
+    ///     The Player class is a Point that may hold more functionality in the
+    ///         future.
+    /// </summary>
+    class Player : Point {
+        /// <summary>
+        ///     The Player constructor takes and passes the required fields to
+        ///         the Point constructor.
+        /// </summary>
+        /// <param name="x">
+        ///     The x coordinate for the Player in the Dungeon.
+        /// </param>
+        /// <param name="y">
+        ///     The y coordinate for the Player in the Dungeon.
+        /// </param>
+        public Player(short x, short y)
+            : base( x, y ) {
+
+        }
+    }
+}
