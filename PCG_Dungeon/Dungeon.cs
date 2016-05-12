6ca201c83c84746b1752c63a966fc07ef1d6ee80
Chris Engstrom <tipher88@gmail.com>
2016-05-12 00:13:27 -0500

    Meant to include Dungeon.cs in commit 9d1f985

--- PCG_Dungeon/Dungeon.cs
+++ PCG_Dungeon/Dungeon.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 
 namespace PCG_Dungeon {
+    /// <summary>
+    ///     The Dungeon class represents the whole playable grid that includes
+    ///         all of the Room%s and Corridors as well as the Player.
+    /// </summary>
     class Dungeon {
-        Random rand;
-
         public enum TileState {
             OPEN = 0,
             WALL = 1,
@@ -15,7 +17,8 @@ namespace PCG_Dungeon {
             END = 4,
         }
 
-        Point player;
+        Random rand;
+        Player player;
 
         // Class globals/consts that shouldn't be changed
         short DUNGEON_WIDTH,
@@ -27,6 +30,25 @@ namespace PCG_Dungeon {
         short[,] board;
         List<Room> roomList;
 
+        /// <summary>
+        ///     The Dungeon constructor takes the required parameters and
+        ///         creates the necessary structures for later use.
+        /// </summary>
+        /// <param name="width">
+        ///     The width of the entire Dungeon.
+        /// </param>
+        /// <param name="height">
+        ///     The height of the entire Dungeon.
+        /// </param>
+        /// <param name="minRoomSize">
+        ///     The minimum size for Room%s in the Dungeon.
+        /// </param>
+        /// <param name="maxRoomSize">
+        ///     The maximum size for Room%s in the Dungeon.
+        /// </param>
+        /// <param name="numRooms">
+        ///     The number of Room%s to attempt to place in the Dungeon.
+        /// </param>
         public Dungeon( short width, short height, short minRoomSize, short maxRoomSize, short numRooms ) {
             DUNGEON_WIDTH = width;
             DUNGEON_HEIGHT = height;
@@ -41,6 +63,10 @@ namespace PCG_Dungeon {
             initializeBoard();
         }
 
+        /// <summary>
+        ///     initializeBoard() sets every tile in the Dungeon to the WALL
+        ///         state.
+        /// </summary>
         private void initializeBoard() {
             for ( int col = 0; col < DUNGEON_WIDTH; col++ ) {
                 for ( int row = 0; row < DUNGEON_HEIGHT; row++ ) {
@@ -49,6 +75,14 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Generates Room%s of varying sizes using the limits passed in to
+        ///         the constructor.
+        /// </summary>
+        /// <param name="endRoomIsNewStartRoom">
+        ///     true = Use the current end Room as the next Dungeon%'s starting Room.<br/>
+        ///     false = Start with a completely new set of Room%s for the next Dungeon.
+        /// </param>
         public void PlaceRooms( bool endRoomIsNewStartRoom ) {
             Room startRoom = null;
 
@@ -100,7 +134,7 @@ namespace PCG_Dungeon {
                             hCorridor( (int)prevCenter.x, (int)newCenter.x, (int)newCenter.y );
                         }
                     } else if ( roomList.Count == 1 ) {
-                        player = new Point( newRoom.center.x, newRoom.center.y );
+                        player = new Player( newRoom.center.x, newRoom.center.y );
                     }
                 }
             }
@@ -108,6 +142,19 @@ namespace PCG_Dungeon {
             updateBoard();
         }
 
+        /// <summary>
+        ///     Creates a horizontal corridor from x1 to x2 using the
+        ///         horizontal coordinate y.
+        /// </summary>
+        /// <param name="x1">
+        ///     The x coordinate for the center of the first Room.
+        /// </param>
+        /// <param name="x2">
+        ///     The x coordinate for the center of the second Room.
+        /// </param>
+        /// <param name="y">
+        ///     The y coordinate that the corridor will run along.
+        /// </param>
         private void hCorridor( int x1, int x2, int y ) {
             for ( int col = Math.Min( x1, x2 ); col < Math.Max( x1, x2 ) + 1; col++ ) {
                 // Remove the wall present
@@ -115,6 +162,19 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Creates a vertical corridor from y1 to y2 using the vertical
+        ///         coordinate x.
+        /// </summary>
+        /// <param name="y1">
+        ///     The y coordinate for the center of the first Room.
+        /// </param>
+        /// <param name="y2">
+        ///     The y coordinate for the center of the second Room.
+        /// </param>
+        /// <param name="x">
+        ///     The x coordinate that the corridor will run along.
+        /// </param>
         private void vCorridor( int y1, int y2, int x ) {
             for ( int row = Math.Min( y1, y2 ); row < Math.Max( y1, y2 ) + 1; row++ ) {
                 // Remove the wall present
@@ -122,6 +182,11 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Goes through the Room%s that have been added to the Dungeon and
+        ///         sets the appropriate tiles to the correct state. Also sets
+        ///         the tile state for the Player%'s location
+        /// </summary>
         private void updateBoard() {
             foreach ( Room room in roomList ) {
                 for ( int col = room.x1; col < room.x2; col++ ) {
@@ -140,12 +205,37 @@ namespace PCG_Dungeon {
             board[player.x, player.y] = (short)TileState.PLAYER;
         }
 
-        // Get the TileState of the position indicated
+        /// <summary>
+        ///     Gets the TileState of the position indicated.
+        /// </summary>
+        /// <param name="col">
+        ///     The x coordinate of the tile to get the TileState for.
+        /// </param>
+        /// <param name="row">
+        ///     The y coordinate of the tile to get the TileState for.
+        /// </param>
+        /// <returns>
+        ///     The numerical value that is associated with the corresponding
+        ///         TileState.
+        /// </returns>
         public int GetPosition( int col, int row ) {
             return board[col, row];
         }
 
-        // Attempt to move the player's piece in a certain direction
+        /// <summary>
+        ///     Attempts to move the Player% in a certain direction.
+        /// </summary>
+        /// <param name="x">
+        ///     The x coordinate of the potential new location.
+        /// </param>
+        /// <param name="y">
+        ///     The y coordinate of the potential new location.
+        /// </param>
+        /// <returns>
+        ///     true = Successfully moved Player<br/>
+        ///     false = Failed to move Player
+        /// </returns>
+        // TODO: This should take a point instead of just x and y
         private bool movePlayer( int x, int y ) {
             short potentialMoveTile = board[player.x + x, player.y - y];
 
@@ -166,30 +256,45 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Attemps to move the Player up one tile.
+        /// </summary>
         public void MovePlayerUp() {
             if ( movePlayer( 0, 1 ) ) {
                 updateBoard();
             }
         }
 
+        /// <summary>
+        ///     Attemps to move the Player right one tile.
+        /// </summary>
         public void MovePlayerRight() {
             if ( movePlayer( 1, 0 ) ) {
                 updateBoard();
             }
         }
 
+        /// <summary>
+        ///     Attemps to move the Player down one tile.
+        /// </summary>
         public void MovePlayerDown() {
             if ( movePlayer( 0, -1 ) ) {
                 updateBoard();
             }
         }
 
+        /// <summary>
+        ///     Attemps to move the Player left one tile.
+        /// </summary>
         public void MovePlayerLeft() {
             if ( movePlayer( -1, 0 ) ) {
                 updateBoard();
             }
         }
 
+        /// <summary>
+        ///     Saves the current Dungeon state out to a text file.
+        /// </summary>
         public void SaveToFile() {
             using ( System.IO.StreamWriter file = new System.IO.StreamWriter( @"..\..\dungeon.txt" ) ) {
                 for ( int row = 0; row < DUNGEON_HEIGHT; row++ ) {
@@ -202,6 +307,10 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Loads a Dungeon state from a text file.
+        /// </summary>
+        // TODO: Get this working
         public void LoadFromFile() {
             using ( System.IO.StreamReader file = new System.IO.StreamReader( @"..\..\dungeon.txt" ) ) {
                 char[] buffer = new char[1];
@@ -222,6 +331,13 @@ namespace PCG_Dungeon {
             }
         }
 
+        /// <summary>
+        ///     Checks to see if the Player has reached the end Room%'s center.
+        /// </summary>
+        /// <returns>
+        ///     true = Dungeon has been completed<br/>
+        ///     false = Dungeon has not been completed yet
+        /// </returns>
         public bool CheckForLevelComplete() {
             if ( player.x == roomList.Last().center.x &&
                  player.y == roomList.Last().center.y ) {
